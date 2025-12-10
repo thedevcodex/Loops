7216c780bbf265c5f8c7cd2e97544db4e2191af0
TheDevJournal <s.sudha19082006@gmail.com>
2025-12-10 20:28:42 +0530

    Create 1_20_odd.py

---
+++ 1_20_odd.py
@@ -0,0 +1,11 @@
+#Print all odd numbers from 1 to 20.
+for i in range(1,21):
+    if i%2==0:
+        pass
+    else:
+        print(i)
+
+  #second way 
+for i in range(1,21):
+    if i%2!=0:
+        print(i)
